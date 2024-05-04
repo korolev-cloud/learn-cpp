51e5e5d0a4238f01c4b379d4e35335263b7ca3d2
korolev-cloud <korol-625-@inbox.ru>
2024-05-04 20:45:26 +0300

    Enjoy

---
+++ 13.1-learn-work/1-arithmetic-mean/1-arithmetic-mean.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Enter the number of numbers: ";
+    int n;
+    std::cin >> n;
+    std::vector<int>vec(n);
+    float s = 0.f;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << i+1 << " numbers ";
+        std::cin >> vec[i];
+        s += vec[i];
+    }
+    std::cout << "Arithmetic mean: " << s / n;
+
+}
+
+// Пользователь вводит с клавиатуры число n, а затем -- n целых чисел.
+// Необходимо вычислить их среднее арифметическое и вывести на экран.
+
