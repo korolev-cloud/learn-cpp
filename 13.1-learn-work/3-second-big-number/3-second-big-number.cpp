36a60f388cc494b0e29c154e4d0e724868ec8039
korolev-cloud <korol-625-@inbox.ru>
2024-05-07 15:52:29 +0300

    Enjoy

--- 13.1-learn-work/3-second-big-number/3-second-big-number.cpp
+++ 13.1-learn-work/3-second-big-number/3-second-big-number.cpp
@@ -1,29 +1,116 @@
-﻿#include <iostream>
-#include <vector>
+﻿#include <chrono>
+
+#include <algorithm>
+#include <iostream>
+
+void insertionSort(float array[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i;
+        while (j > 0 && array[j - 1] < array[j])
+        {
+            std::swap(array[j - 1], array[j]);
+            j--;
+        }
+    }
+}
+
+void bubbleSort(float array[], int size)
+{
+    bool b = true;
+    while (b)
+    {
+        b = false;
+        for (int i = 0; i < size - 1; i++)
+        {
+            // Егор
+                if (array[i] < array[i + 1])
+                {
+                    std::swap(array[i], array[i + 1]);
+                    b = true;
+                }
+        }
+    }
+}
+
+void shakeSort(float array[], int size)
+{
+    bool b = true;
+    int beg = 0 - 1; // Imaginative int l = 0.
+    int end = size - 1;
+
+    while (b)
+    {
+        b = false;
+        beg++;
+        for (int i = beg; i < end; i++)
+        {
+            if (array[i] < array[i + 1])
+            {
+                std::swap(array[i], array[i + 1]);
+                b = true;
+            }
+        }
+        if (!b)
+            break;
+
+        end--;
+
+        for (int i = end; i > beg; i--)
+        {
+            if (array[i] > array[i - 1])
+            {
+                std::swap(array[i], array[i - 1]);
+                b = true;
+            }
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Enter the number of numbers: ";
-    int n;
-    std::cin >> n;
-    std::vector<int>vec(n);
-    for (int i = 0; i < n; i++)
+    srand(time(NULL));
+    const int r = 10'000; // 1'000'000;
+    float numbers[r];
+
     {
-        std::cin >> vec[i];
+        for (int i = 0; i < r; i++)
+            numbers[i] = rand();
+
+        auto start = std::chrono::system_clock::now();
+        insertionSort(numbers, r);
+        std::cout << "insertionSort - " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << std::endl;
     }
-    
-    std::vector<int>bigNum(2,0);
-    for (int i = 0; i < n; i++)
+
     {
-        if (vec[i] > bigNum[0]) 
-        {
-            bigNum[1] = bigNum[0];
-            bigNum[0] = vec[i];
-        }
-        else if (vec[i] > bigNum[1]) bigNum[1] = vec[i];
+        for (int i = 0; i < r; i++)
+            numbers[i] = rand();
+
+        auto start = std::chrono::system_clock::now();
+        bubbleSort(numbers, r);
+        std::cout << "bubbleSort - " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << std::endl;
     }
-    std::cout << bigNum[1];
+
+    {
+        for (int i = 0; i < r; i++)
+            numbers[i] = rand();
+
+        auto start = std::chrono::system_clock::now();
+        shakeSort(numbers, r);
+        std::cout << "shakeSort - " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << std::endl;
+    }
+
+    {
+        for (int i = 0; i < r; i++)
+            numbers[i] = rand();
+
+        auto start = std::chrono::system_clock::now();
+        std::sort(numbers, &numbers[r]);
+        std::cout << "std::sort - " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << std::endl;
+    }
 }
+
 
 // Пользователь вводит с клавиатуры число n, а затем -- n целых чисел. 
 // Необходимо отыскать второе по величине из этих чисел и вывести на экран.
