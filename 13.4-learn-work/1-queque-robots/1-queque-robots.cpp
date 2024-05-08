9bc3f8b832b532740630721ad5f1e8d2386ac1e0
korolev-cloud <korol-625-@inbox.ru>
2024-05-08 11:25:14 +0300

    Enjoy

---
+++ 13.4-learn-work/1-queque-robots/1-queque-robots.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vec(0);
+	int num = 0;
+	while (num != -1)
+	{
+		cout << "Enter number ";
+		cin >> num;
+		assert(int(num));
+		vec.push_back(num);
+		
+	}
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+	cout << endl;
+}
+
+/* С помощью push_back() реализуйте очередь из роботов-андроидов. 
+Роботы приходят в очередь и называют(вводят с клавиатуры) свои порядковые номера 
+(целые положительные). Последний робот в очереди всегда имеет номер -1. 
+Когда он пришёл, необходимо вывести на экран все номера в очереди.
+*/
