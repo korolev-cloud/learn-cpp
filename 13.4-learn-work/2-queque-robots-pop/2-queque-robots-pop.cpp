6d9937d8dbc970c0112e029eb9be21203e1249f4
korolev-cloud <korol-625-@inbox.ru>
2024-05-08 11:56:36 +0300

    Enjoy

---
+++ 13.4-learn-work/2-queque-robots-pop/2-queque-robots-pop.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vec(0);
+	int num = 0;
+	cout << "Enter number ";
+	cin >> num;
+	vec.push_back(num);
+	while (num != -1)
+	{
+		cout << "Enter number ";
+		cin >> num;
+		assert(int(num));
+		if (num >= vec[vec.size() - 1]) vec.pop_back();
+		vec.push_back(num);
+	}
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+	cout << endl;
+}
+
+/*
+Робот приходит в очередь и заставляет уходить из конца очереди всех,
+ чей номер меньше, чем у него, а потом становится в очередь сам.
+ Последний робот будет иметь номер - 1 (и, соответственно, не сможет никого выгнать,
+ так как остальные имеют положительные номера).Когда он пришёл, нужно вывести все номера в очереди на экран.
+*/ 
