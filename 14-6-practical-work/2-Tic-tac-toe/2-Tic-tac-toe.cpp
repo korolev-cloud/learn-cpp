5134d02fab1b080dc0eb1e472e8f7583e2e5bd2d
Egor <128840944+korolev-cloud@users.noreply.github.com>
2024-06-19 07:35:12 +0300

    Added diagonal checking

--- 14-6-practical-work/2-Tic-tac-toe/2-Tic-tac-toe.cpp
+++ 14-6-practical-work/2-Tic-tac-toe/2-Tic-tac-toe.cpp
@@ -4,7 +4,9 @@ char checkWin(char (&field)[3][3])
 {
     for (int i = 0; i < 3; i++) {
         if (field[i][0] == field[i][1] && field[i][0] == field[i][2]) return field[i][0];
-        else if (field[0][i] == field[1][i] && field[0][i] == field[2][i]) return field[0][i]; 
+        else if (field[0][i] == field[1][i] && field[0][i] == field[2][i]) return field[0][i];
+        else if (field[0][0] == field[1][1] && field[0][0] == field[2][2]) return field[0][0]; 
+        else if (field[0][2] == field[1][1] && field[0][2] == field[2][0]) return field[0][2]; 
     }
     return ' ';
 }
