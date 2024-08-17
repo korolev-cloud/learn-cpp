341e9da5885a1271900ada59b157cfb9993f6d7b
korolev-cloud <korol-625-@inbox.ru>
2024-08-17 09:30:12 +0300

    Correct sum

--- 15-6-practical-work/1-element-sum/1-element-sum.cpp
+++ 15-6-practical-work/1-element-sum/1-element-sum.cpp
@@ -2,7 +2,26 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4, 6, 9};
+    int startPos = 0;
+    int endPos = sizeof(a) / sizeof(a[0]) - 1;
+    int sum = 0;
+    for (int j = 0; j < endPos; j++) {
+        int currentSum = 0;
+        for (int i = startPos; i < j; i++) {
+            currentSum += a[i];
+        }
+        if (currentSum < sum) startPos++;
+        else sum = currentSum;
+        if (a[startPos] + a[startPos + 1] < 0) startPos += 2;
+        if ((a[endPos] + a[endPos - 1]) < 0) endPos -= 2;
+        sum += a[j];
+    }
+    
+    std::cout << "Sum = " << sum << "\n";
+    std::cout << "startPos = " << startPos << "\n";
+    std::cout << "endPos = " << endPos << "\n";
+
 }
 
 /*
