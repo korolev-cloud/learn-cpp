71bd22540b21bb8b08eb7829acef917cf5ccadb3
korolev-cloud <korol-625-@inbox.ru>
2025-04-30 09:53:44 +0300

    Enjoy tasks

---
+++ 16-6-practical-work/3-Calculator/3-Calculator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <sstream>
+
+int main(){
+    std::string buffer;
+    std::cout << "Input string in format <number-1><action><number-2>: ";
+    std::cin >> buffer;
+    double firtstNumber = 0.f;
+    double secondNumber = 0.f;
+    char operation;
+    std::stringstream buffer_stream(buffer);
+    buffer_stream >> firtstNumber >> operation >> secondNumber;
+    if (operation == '+') std::cout << "Solution: " << (firtstNumber + secondNumber);
+    else if (operation == '-') std::cout << "Solution: " << (firtstNumber - secondNumber);
+    else if (operation == '*') std::cout << "Solution: " << (firtstNumber * secondNumber);
+    else if (operation == '/') std::cout << "Solution: " << (firtstNumber / secondNumber);
+    return 0;
+}
+
+/*
+Что нужно сделать
+Создайте небольшую программу для вычисления простых действий с числами. 
+При запуске программа ожидает пользовательского ввода во временную переменную строку. 
+После ввода строки она распарсивается на отдельные члены. 
+Строка записывается в форме “<число-1><действие><число-2>” (без пробелов). 
+Оба числа — это значения с плавающей точкой, повышенной точности (double). 
+Действие может быть одним из: +, −, /, *. Результат действия выводится в стандартный вывод cout.
+
+Рекомендации
+Чтобы разделить полученную строку на отдельные элементы — числа и оператор, 
+воспользуйтесь строковым потоком. Для этого создайте пустую строку std::string buffer 
+и дайте пользователю возможность при помощи std::cin ввести в неё команду. 
+Затем оберните полученную строку так: std::stringstream buffer_stream(buffer); 
+Это позволит вам  получить каждый из трёх компонентов команды: buffer_stream >> a >> operation >> b;
+
+
+
+*/
